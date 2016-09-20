0003a8a780b77681766d3fcafdb031385125d417
Fotache, Dragos <Dragos.Fotache@awinta.de>
2016-09-20 17:28:34 +0300

    initial

---
+++ app1.ts
@@ -0,0 +1,7 @@
+/// <reference path="typings/index.d.ts" />
+
+import * as express from "express";
+import * as path from "path";
+import * as logger from "morgan";
+import * as favicon from "serve-favicon";
+import * as cookieParser from "cookie-parser";
