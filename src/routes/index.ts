53f7cb564827e396445d8d50371436fc22f8d051
Dragos <dragosfs@gmail.com>
2016-10-09 22:17:10 +0300

    .

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,24 +3,40 @@
 import * as express from "express";
 import * as mysql from "mysql";
 
-var connection = mysql.createConnection({
-  host     : 'us-cdbr-iron-east-04.cleardb.net',
-  user     : 'bb8476f17060ad',
-  password : '35ae4421',
-  database : 'heroku_1a9009448f5bede'
-});
 
+var db_config = {
+    host: 'localhost',
+    user: 'root',
+    password: '1234',
+    database: 'test'
+};
+
+// var db_config = {
+//     host: 'us-cdbr-iron-east-04.cleardb.net',
+//     user: 'bb8476f17060ad',
+//     password: '35ae4421',
+//     database: 'heroku_1a9009448f5bede'
+// };
+
+var connection: mysql.IConnection;
 
 function index(req: express.Request, res: express.Response, next: express.NextFunction) {
-  connection.connect();
 
-  connection.query("Select * from test",
-    function (err, result) {
-      if (err) throw err;
-      res.json(result);
+    connection = mysql.createConnection(db_config);
+
+    connection.query("Select * from users",
+        function (err, result) {
+            if (err) throw err;
+            res.json(result);
+        });
+
+    connection.on('error', function (err) {
+        console.log(err);
+        if (err.code === 'PROTOCOL_CONNECTION_LOST') {
+        } else {
+            throw err;
+        }
     });
-
-  connection.end();
 }
 
 var router: express.Router = express.Router();
